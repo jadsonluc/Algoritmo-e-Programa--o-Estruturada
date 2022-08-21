6e9e1ec3b1d45eb435c2af9475b0b87b8ee52c0e
Jadson Lucas <jadsonjlas@gmail.com>
2022-08-21 19:07:36 -0300

    ok

--- somar.c
+++ somar.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
 int somar(int a, int b){
-    return 
+    return a + b;
+}
+
+int main() {
+    int result;
+    result = somar(10, 12);
+    printf("\n Resultado da soma = %d", result);
+
+    return 0;
 }
