ac947ddc29cce366466153494f277196c1d196a0
Jadson Lucas <jadsonjlas@gmail.com>
2022-08-26 20:11:36 -0300

    ok

--- prova.c
+++ prova.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 
-int main () {
-    int idade = 18;
-    int *ponteiro_para_idade = &idade;
-    printf("\n Conteudo do ponteiro: %p", ponteiro_para_idade);
+const int valor = 3;
+
+int main ()
+
+{
+
+int vetor[4]={2,4,6,8};
+
+vetor [0] = vetor [0] * valor;
+
+vetor [1] = vetor [1] * valor;
+
+vetor [2] = vetor [2] * valor;
+
+vetor [3] = vetor [3] * valor;
+
+printf ("\n  %d - ", vetor [3]);
+
+printf ("\n  %d -", vetor [2]);
+
+printf ("\n  %d -", vetor [1]);
+
+printf ("\n  %d .", vetor [0]);
+
+return 0;
+
 }
